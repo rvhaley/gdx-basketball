7b4aa2f75f819bd93260a9b3d1a0ffeda85ec38b
Richard Haley III <rvhaley@hotmail.com>
2017-01-02 17:56:14 -0800

    New class, Constants.java. Changes to WorldRenderer.java class.

---
+++ core/src/com/tryla_g/basketball/game/Constants.java
@@ -0,0 +1,11 @@
+package com.tryla_g.basketball.game;
+
+public class Constants {
+
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+	
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	
+}
--- core/src/com/tryla_g/basketball/game/WorldRenderer.java
+++ core/src/com/tryla_g/basketball/game/WorldRenderer.java
@@ -1,19 +1,44 @@
 package com.tryla_g.basketball.game;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 
 public class WorldRenderer implements Disposable {
 
 	private static final String TAG = WorldRenderer.class.getName();
 	
-	public WorldRenderer() {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private WorldController worldController;
+	
+	public WorldRenderer(WorldController worldController) {
+		this.worldController = worldController;
+		init();
+	}
+	
+	private void init() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+		camera.update();
+	}
+	
+	public void render() {
+		renderWorld(batch);
+	}
+	
+	private void renderWorld(SpriteBatch batch) {
+		
+	}
+	
+	public void resize(int width, int height) {
 		
 	}
 	
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		batch.dispose();
 	}
 
 }
