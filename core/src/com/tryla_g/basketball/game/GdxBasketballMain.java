cad0ecf73e25532d20053d4bdaedbea2263ae5fe
Richard Haley III <rvhaley@hotmail.com>
2017-01-02 18:18:28 -0800

    Added implementations for all ApplicationListener methods in class GdxBasketballMain.java.

--- core/src/com/tryla_g/basketball/game/GdxBasketballMain.java
+++ core/src/com/tryla_g/basketball/game/GdxBasketballMain.java
@@ -3,6 +3,7 @@ package com.tryla_g.basketball.game;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class GdxBasketballMain implements ApplicationListener {
 
@@ -27,7 +28,7 @@ public class GdxBasketballMain implements ApplicationListener {
 
 	@Override
 	public void resize(int width, int height) {
-		
+		worldRenderer.resize(width, height);
 	}
 
 	@Override
@@ -35,21 +36,25 @@ public class GdxBasketballMain implements ApplicationListener {
 		if (!paused) {
 			worldController.update(Gdx.graphics.getDeltaTime());
 		}
+		
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		worldRenderer.render();
 	}
 
 	@Override
 	public void pause() {
-		
+		paused = true;
 	}
 
 	@Override
 	public void resume() {
-		
+		paused = false;
 	}
 
 	@Override
 	public void dispose() {
-				
+		worldRenderer.dispose();
 	}
 
 }
